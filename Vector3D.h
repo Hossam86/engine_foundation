49b67e36a8c749cb18d71511c52f904418beda3b
Hossam Samir <hossamsamir@outlook.com>
2023-01-05 17:10:58 +0200

    vector components

---
+++ Vector3D.h
@@ -0,0 +1,30 @@
+//
+// Created by Hossam Samir on 05/01/2023.
+//
+
+#ifndef GAME_ENGINE_MATH_VECTOR3D_H
+#define GAME_ENGINE_MATH_VECTOR3D_H
+
+
+struct Vector3D {
+    float x, y, z;
+
+    Vector3D() = default;
+
+    Vector3D(float a, float b, float c) {
+        x = a;
+        y = b;
+        z = c;
+    }
+
+    float &operator[](int i) {
+        return (&x)[i];
+    }
+
+    const float &operator[](int i) const {
+        return (&x)[i];
+    }
+
+};
+
+#endif //GAME_ENGINE_MATH_VECTOR3D_H
--- main.cpp
+++ main.cpp
@@ -1,6 +1,16 @@
 #include <iostream>
+#include "Vector3D.h"
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    Vector3D x{};
+    std::cout << "vector components" << "(" << x.x << " ," << x.y << " ," << x.z << ")\n";
+    std::cout << "vector components" << "(" << x[0] << " ," << x[1] << " ," << x[2] << ")\n";
+
+    x[0] = 1;
+    x[1] = 2;
+    x[2] = 3;
+
+    std::cout << "vector components" << "(" << x.x << " ," << x.y << " ," << x.z << ")\n";
+    std::cout << "vector components" << "(" << x[0] << " ," << x[1] << " ," << x[2] << ")\n";
     return 0;
 }
