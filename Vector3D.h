020fdefd9ea8e72bb0bdc72a7d67945e918ccb19
Hossam Samir <hossamsamir@outlook.com>
2023-01-06 17:26:28 +0200

    vector magnitude amd normalize funcs

--- Vector3D.h
+++ Vector3D.h
@@ -42,4 +42,25 @@ struct Vector3D {
 
 };
 
+inline Vector3D operator*(const Vector3D &v, float s) {
+    return Vector3D{v.x * s, v.y * s, v.z * s};
+}
+
+inline Vector3D operator/(const Vector3D &v, float s) {
+    s = 1.0F / s;
+    return Vector3D{v.x * s, v.y * s, v.z * s};
+}
+
+inline Vector3D operator-(const Vector3D &v) {
+    return Vector3D{-v.x, -v.y, -v.z};
+}
+
+inline float Magnitude(const Vector3D &v) {
+    return sqrt(v.x * v.x + v.y * v.y + v.z * v.z);
+}
+
+inline Vector3D Normlize(const Vector3D &v) {
+    return (v / Magnitude(v));
+}
+
 #endif //GAME_ENGINE_MATH_VECTOR3D_H
