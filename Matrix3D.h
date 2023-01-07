a684dda12dbee1d4dae727b5da65efc24b503a0a
Hossam Samir <hossamsamir@outlook.com>
2023-01-07 19:18:16 +0200

    Matrix3D multiplication operators

--- Matrix3D.h
+++ Matrix3D.h
@@ -37,7 +37,8 @@ public:
         n[2][0] = c.x;
         n[2][1] = c.y;
         n[2][2] = c.z;
-    };
+    }
+
 
     float &operator()(int i, int j) {
         return n[j][i];
@@ -52,9 +53,29 @@ public:
     }
 
     const Vector3D &operator[](int j) const {
-        return (*reinterpret_cast<Vector3D *>(n[j]));
+        return (*(reinterpret_cast<const Vector3D *>(n[j])));
     }
-
 };
+
+
+Matrix3D operator*(const Matrix3D &a, const Matrix3D &b) {
+    return Matrix3D(a(0, 0) * b(0, 0) + a(0, 1) * b(1, 0) + a(0, 2) * b(2, 0),
+                    a(0, 0) * b(0, 1) + a(0, 1) * b(1, 1) + a(0, 2) * b(2, 2),
+                    a(0, 0) * b(0, 2) + a(0, 1) * b(1, 2) + a(0, 2) * b(2, 2),
+
+                    a(1, 0) * b(0, 0) + a(1, 1) * b(1, 0) + a(1, 2) * b(2, 0),
+                    a(1, 0) * b(0, 1) + a(1, 1) * b(1, 1) + a(1, 2) * b(2, 2),
+                    a(1, 0) * b(0, 2) + a(1, 1) * b(1, 2) + a(1, 2) * b(2, 2),
+
+                    a(2, 0) * b(0, 0) + a(2, 1) * b(1, 0) + a(2, 2) * b(2, 0),
+                    a(2, 0) * b(0, 1) + a(2, 1) * b(1, 1) + a(2, 2) * b(2, 2),
+                    a(2, 0) * b(0, 2) + a(2, 1) * b(1, 2) + a(2, 2) * b(2, 2));
+}
+
+Vector3D operator*(const Matrix3D &m, const Vector3D &v) {
+    return Vector3D(m(0, 0) * v.x + m(0, 1) * v.y + m(0, 2) * v.z,
+                    m(1, 0) * v.x + m(1, 1) * v.y + m(1, 2) * v.z,
+                    m(2, 0) * v.x + m(2, 1) * v.y + m(2, 2) * v.z);
+}
 
 #endif //GAME_ENGINE_MATH_MATRIX3D_H
